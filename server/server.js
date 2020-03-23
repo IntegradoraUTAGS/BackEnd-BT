fc0829499dcc72c7640ccfffc1c628b7ca9d902a
unknown <ivanperezjimenez1L40@outlook.es>
2020-03-23 17:26:38 -0600

    Actualizacion

--- server/server.js
+++ server/server.js
@@ -1,10 +1,9 @@
 require('./config/bd');
 const express = require('express');
 const mongoose = require('mongoose');
+
 const app = express();
 const bodyparser = require('body-parser');
-
-
 
 
 // parse aplication/x-www.form-uelencoded
@@ -13,18 +12,23 @@ app.use(bodyparser.urlencoded({ extended: false }));
 //parse formato a application/json
 app.use(bodyparser.json());
 
+
 // Habilita CORS
 app.use((req, res, next) => {
+
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader(
         'Access-Control-Allow-Headers',
         'Origin, X-Requested-With, Content-Type, Accept, Authorization, token'
     );
+
     res.setHeader(
         'Access-Control-Allow-Methods',
         'GET, POST, PATCH, PUT, DELETE, OPTIONS'
     );
     next();
+
+
 });
 
 
@@ -46,4 +50,5 @@ mongoose.connect('mongodb+srv://Admin:BolsaUT123@cluster0-qgmkk.mongodb.net/Bols
     });
 
 app.listen(process.env.PORT, () => {
-            console.log('Escuchando por el puerto', process.env.PORT);
+    console.log('Escuchando por el puerto', process.env.PORT)
+});
