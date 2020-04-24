c7770b20f9e25ab94d468d664c42b82f198699f7
Ceslusbel <ceslusbel@gmail.com>
2020-04-23 20:42:58 -0500

    cambio ip + puertos para el servidor

--- server/config/bd.js
+++ server/config/bd.js
@@ -1,5 +1,5 @@
 // PUERTO
-process.env.PORT = process.env.PORT || 3000;
+process.env.PORT = process.env.PORT || 6399;
 //ENTORNO
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
@@ -8,7 +8,7 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 let urlDB;
 
 if (process.env.NODE_ENV === 'dev') {
-    urlDB = 'http://localhost:3000';
+    urlDB = 'http://172.17.1.7:6399';
 }
 process.env.URLDB = urlDB;
 // FIRMA DE JWT
